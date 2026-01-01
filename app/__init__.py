4fea0423c147c9809ee5143fefd43e4275b19ab2
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-01-01 12:24:30 +0000

    Define skeleton of app

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
 """
 Creates a Flask app, registers routes and initializes database.
 """
+
+def create_app():
+    pass
--- run.py
+++ run.py
@@ -2,3 +2,11 @@
 Entry point of the application.
 Responsible only for starting the app.
 """
+
+from app import create_app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    print("App starting...")
